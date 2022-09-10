ac64106ff574efdbbe453fbc8343d6cdedd61395
thephenomenonu <108215785+thephenomenonu@users.noreply.github.com>
2022-09-10 13:01:09 +0530

    Test commit

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { FontawesomeObject } from "@fortawesome/fontawesome-svg-core";
+// import { FontawesomeObject } from "@fortawesome/fontawesome-svg-core";
 
 export default function Icons() {
     return (
         <div class="icon-bar">
-            <FontAwesomeIcon icon="fa-brands fa-square-facebook" />
+            {/* <FontAwesomeIcon icon="fa-brands fa-square-facebook" /> */}
         </div>
     )
 }
