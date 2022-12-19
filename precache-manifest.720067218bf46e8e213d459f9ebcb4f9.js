db303500f4041cbe3f6da672dcefbb20d9e84d5e
thephenomenonu <108215785+thephenomenonu@users.noreply.github.com>
2022-12-19 21:28:39 +0530

    Updates

---
+++ precache-manifest.720067218bf46e8e213d459f9ebcb4f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da658d2caba41137fab",
+    "url": "/anime-quote-generator/static/css/main.6c7cf67d.chunk.css"
+  },
+  {
+    "revision": "9da658d2caba41137fab",
+    "url": "/anime-quote-generator/static/js/main.9da658d2.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/anime-quote-generator/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "46aed6a93283ab65c298",
+    "url": "/anime-quote-generator/static/js/2.46aed6a9.chunk.js"
+  },
+  {
+    "revision": "c00451c293f2a9a80d62",
+    "url": "/anime-quote-generator/static/js/runtime~main.c00451c2.js"
+  },
+  {
+    "revision": "02d3cc317226571872e0c13fdfa54542",
+    "url": "/anime-quote-generator/index.html"
+  }
+];
